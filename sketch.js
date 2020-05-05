74d248fe560444a4dfadf34ef2e2c68879ad4346
Shreesha <archhr@gmail.com>
2020-05-05 19:27:09 +0530

    changed platform

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform = new Ground(140, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -40,7 +40,7 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
-    Engine.update(engine);
+	Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
